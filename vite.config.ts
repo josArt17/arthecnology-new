0140a97cda898134622b6177c27b1a128cc32c0a
Jose Angel Arteaga <josart1296@gmail.com>
2025-02-18 20:13:15 -0600

    adding principal page

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        principal: resolve(__dirname, "src/principal/main.html"),
       },
     },
   },
